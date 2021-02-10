c8b2b64239d3bab141136722e3d27e95e8875838
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-10 20:58:42 +0000

    Send a username

--- client.js
+++ client.js
@@ -7,6 +7,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect', function() {
+    console.log('Successfully connected to game server');
+    conn.write('Name: TW7');
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
