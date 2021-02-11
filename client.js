33c87cfb91e4c6f4aeb9992e7dd20118141973d2
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-11 02:23:58 +0000

    Moved two constant values (IP and PORT) to separate file

--- client.js
+++ client.js
@@ -1,22 +1,22 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
-  // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('connect', function() {
     console.log('Successfully connected to game server');
     conn.write('Name: TW7');
-    // conn.write('Move: up');
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
   return conn;
 };
+
 
 
 module.exports = {
